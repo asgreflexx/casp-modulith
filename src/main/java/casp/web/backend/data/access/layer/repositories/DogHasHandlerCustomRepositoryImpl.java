e1e868d5000e794c783b72a49b217248941c7071
Luis Ribeiro <lacribeiro11@gmail.com>
2024-09-06 22:53:00 +0200

    #1 Refactor DogHasHandlerCustomRepositoryImpl

--- src/main/java/casp/web/backend/data/access/layer/repositories/DogHasHandlerCustomRepositoryImpl.java
+++ src/main/java/casp/web/backend/data/access/layer/repositories/DogHasHandlerCustomRepositoryImpl.java
@@ -2,56 +2,71 @@ package casp.web.backend.data.access.layer.repositories;
 
 import casp.web.backend.data.access.layer.documents.dog.Dog;
 import casp.web.backend.data.access.layer.documents.dog.DogHasHandler;
+import casp.web.backend.data.access.layer.documents.dog.QDog;
 import casp.web.backend.data.access.layer.documents.dog.QDogHasHandler;
 import casp.web.backend.data.access.layer.documents.enumerations.EntityStatus;
 import casp.web.backend.data.access.layer.documents.member.Member;
 import casp.web.backend.data.access.layer.documents.member.QMember;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.repository.support.SpringDataMongodbQuery;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Component
 class DogHasHandlerCustomRepositoryImpl implements DogHasHandlerCustomRepository {
+    private static final QMember MEMBER = QMember.member;
+    private static final QDogHasHandler DOG_HAS_HANDLER = QDogHasHandler.dogHasHandler;
+    private static final QDog DOG = QDog.dog;
     private final MongoOperations mongoOperations;
-    private final DogRepository dogRepository;
 
     @Autowired
-    DogHasHandlerCustomRepositoryImpl(final MongoOperations mongoOperations, final DogRepository dogRepository) {
+    DogHasHandlerCustomRepositoryImpl(final MongoOperations mongoOperations) {
         this.mongoOperations = mongoOperations;
-        this.dogRepository = dogRepository;
     }
 
+    // Query DBRef with Spring Data MongoDB using QueryDSL -> It's not supported.
+    // https://stackoverflow.com/a/44316748/1066054
     @Transactional(readOnly = true)
     @Override
     public Set<DogHasHandler> findAllByMemberNameOrDogName(final String name) {
-        var query = new SpringDataMongodbQuery<>(mongoOperations, DogHasHandler.class);
-        var dogHasHandler = QDogHasHandler.dogHasHandler;
-        var expression = dogHasHandler.entityStatus.eq(EntityStatus.ACTIVE);
-        if (ObjectUtils.isNotEmpty(name)) {
-            expression = expression.and(dogHasHandler.memberId.in(getMemberIds(name)).or(dogHasHandler.dogId.in(getDogIds(name))));
+        var expression = DOG_HAS_HANDLER.entityStatus.eq(EntityStatus.ACTIVE);
+        if (StringUtils.hasText(name)) {
+            var dogsId = getDogsId(name);
+            var membersId = getMembersId(name);
+            expression = expression.and(DOG_HAS_HANDLER.dogId.in(dogsId).or(DOG_HAS_HANDLER.memberId.in(membersId)));
         }
-        return query.where(expression).stream().collect(Collectors.toSet());
-    }
-
-    private Set<UUID> getDogIds(final String name) {
-        return dogRepository.findAllByEntityStatusAndNameIgnoreCase(EntityStatus.ACTIVE, name)
+        return createDogHasHandlerQuery()
+                .where(expression)
                 .stream()
-                .map(Dog::getId)
                 .collect(Collectors.toSet());
     }
 
-    private Set<UUID> getMemberIds(final String name) {
-        var query = new SpringDataMongodbQuery<>(mongoOperations, Member.class);
-        var member = QMember.member;
-        var expression = member.entityStatus.eq(EntityStatus.ACTIVE)
-                .and(member.firstName.equalsIgnoreCase(name).or(member.lastName.equalsIgnoreCase(name)));
-        return query.where(expression).stream().map(Member::getId).collect(Collectors.toSet());
+    private List<UUID> getMembersId(final String name) {
+        return new SpringDataMongodbQuery<>(mongoOperations, Member.class)
+                .where(MEMBER.entityStatus.eq(EntityStatus.ACTIVE)
+                        .and(MEMBER.firstName.containsIgnoreCase(name).or(MEMBER.lastName.containsIgnoreCase(name))))
+                .stream()
+                .map(Member::getId)
+                .toList();
+    }
+
+    private List<UUID> getDogsId(final String name) {
+        return new SpringDataMongodbQuery<>(mongoOperations, Dog.class)
+                .where(DOG.entityStatus.eq(EntityStatus.ACTIVE)
+                        .and(DOG.name.containsIgnoreCase(name)))
+                .stream()
+                .map(Dog::getId)
+                .toList();
+    }
+
+    private SpringDataMongodbQuery<DogHasHandler> createDogHasHandlerQuery() {
+        return new SpringDataMongodbQuery<>(mongoOperations, DogHasHandler.class);
     }
 }
