1b0dc9694c6b180e2d3594e31f81732149d5c89b
Luis Ribeiro <lacribeiro11@gmail.com>
2024-09-07 18:09:17 +0200

    #1 Add OpenApiConfig

---
+++ src/main/java/casp/web/backend/presentation/layer/configuration/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package casp.web.backend.presentation.layer.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "AdminV2 Service APIs"),
+        servers = {@Server(url = "${springdoc.test-server.url}/adminV2", description = "${springdoc.test-server.description}")},
+        security = {@SecurityRequirement(name = "bearer-key")})
+@Profile("!dev")
+class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+    }
+}
