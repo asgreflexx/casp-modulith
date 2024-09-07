82ad1f542f4728c22144a1cc3b4426efc8561c98
Luis Ribeiro <lacribeiro11@gmail.com>
2024-09-07 18:09:17 +0200

    #1 Add OpenApiConfig

--- src/main/java/casp/web/backend/AdminApplication.java
+++ src/main/java/casp/web/backend/AdminApplication.java
@@ -1,16 +1,8 @@
 package casp.web.backend;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -18,20 +10,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
-@OpenAPIDefinition(info = @Info(title = "Admin Service APIs"),
-        servers = {@Server(url = "${springdoc.test-server.url}/admin", description = "${springdoc.test-server.description}")},
-        security = {@SecurityRequirement(name = "bearer-key")})
 public class AdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
-    }
-
-    @Bean
-    OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
     }
 }
