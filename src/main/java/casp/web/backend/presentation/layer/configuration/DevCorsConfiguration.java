fee012a20d4908edf0d73d41098a4c7796ae52ba
Luis Ribeiro <lacribeiro11@gmail.com>
2024-09-07 18:10:51 +0200

    #1 Add DevCorsConfiguration

---
+++ src/main/java/casp/web/backend/presentation/layer/configuration/DevCorsConfiguration.java
@@ -0,0 +1,16 @@
+package casp.web.backend.presentation.layer.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("dev")
+class DevCorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "DELETE");
+    }
+}
