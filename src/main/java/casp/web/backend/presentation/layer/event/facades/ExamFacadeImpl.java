adba861eb2941af02b425b190d9335525246d433
Luis Ribeiro <lacribeiro11@gmail.com>
2024-09-27 20:06:09 +0200

    #1 Add getAllByYear to ExamFacade

--- src/main/java/casp/web/backend/presentation/layer/event/facades/ExamFacadeImpl.java
+++ src/main/java/casp/web/backend/presentation/layer/event/facades/ExamFacadeImpl.java
@@ -9,6 +9,8 @@ import casp.web.backend.presentation.layer.dtos.event.types.ExamDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -66,6 +68,12 @@ class ExamFacadeImpl implements ExamFacade {
     @Override
     public void deleteById(final UUID id) {
         examService.deleteById(id);
+    }
+
+    @Override
+    public Page<ExamDto> getAllByYear(final int year, final Pageable pageable) {
+        var examPage = examService.getBaseEventsAsPage(year, pageable);
+        return EXAM_MAPPER.toDtoPage(examPage);
     }
 
     private void setExamParticipants(final ExamDto examDto) {
--- src/test/java/casp/web/backend/presentation/layer/event/facades/ExamFacadeImplTest.java
+++ src/test/java/casp/web/backend/presentation/layer/event/facades/ExamFacadeImplTest.java
@@ -17,6 +17,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Set;
@@ -60,6 +62,20 @@ class ExamFacadeImplTest {
         examFacade.deleteById(id);
 
         verify(examService).deleteById(id);
+    }
+
+    @Test
+    void getAllByYear() {
+        int year = 2023;
+        var pageable = Pageable.unpaged();
+        var exam = TestFixture.createExam();
+        when(examService.getBaseEventsAsPage(year, pageable)).thenReturn(new PageImpl<>(List.of(exam)));
+
+        var examPage = examFacade.getAllByYear(year, pageable);
+
+        assertThat(examPage.getContent())
+                .singleElement()
+                .satisfies(examDto -> assertEquals(exam.getId(), examDto.getId()));
     }
 
     @Nested
