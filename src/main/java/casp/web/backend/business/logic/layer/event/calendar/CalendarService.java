049688fc103c4649bbef0d6bfbd103a8e742bf4c
Luis Ribeiro <lacribeiro11@gmail.com>
2024-10-02 10:38:56 +0200

    #1 Add Documentation to CalendarService

--- src/main/java/casp/web/backend/business/logic/layer/event/calendar/CalendarService.java
+++ src/main/java/casp/web/backend/business/logic/layer/event/calendar/CalendarService.java
@@ -11,10 +11,22 @@ import java.util.UUID;
 
 public interface CalendarService {
 
+    /**
+     * Get an active calendar entry by id or throws an exception if not found.
+     *
+     * @param id the id of the calendar entry
+     * @return the calendar entry, or throws an exception if the calendar entry is not found
+     */
     Calendar getCalendarEntryById(UUID id);
 
     List<Calendar> getCalendarEntriesByPeriodAndEventTypes(LocalDate calendarEntriesFrom, LocalDate calendarEntriesTo, @NotNull Set<String> eventTypes);
 
+    /**
+     * Delete all calendar entries for a given base event, creates new calendar entries and sets BaseEvent#minLocalDateTime and BaseEvent#maxLocalDateTime.
+     *
+     * @param baseEvent       the base event for which the calendar entries should be replaced
+     * @param calendarEntries the new calendar entries
+     */
     void replaceCalendarEntries(BaseEvent baseEvent, List<Calendar> calendarEntries);
 
     List<Calendar> getCalendarEntriesByBaseEvent(BaseEvent baseEvent);
@@ -23,10 +35,28 @@ public interface CalendarService {
 
     void deleteCalendarEntryById(UUID id);
 
+    /**
+     * Set all calendar entries status with the given base event id to delete.
+     * This is used when a base event is deleted.
+     *
+     * @param baseEventId the id of the base event
+     */
     void deleteCalendarEntriesByBaseEventId(UUID baseEventId);
 
+    /**
+     * Set all calendar entries status with the given base event id to inactive.
+     * This is used when a base event is deactivated.
+     *
+     * @param baseEventId the id of the base event
+     */
     void deactivateCalendarEntriesByBaseEventId(UUID baseEventId);
 
+    /**
+     * Set all calendar entries status with the given base event id to active.
+     * This is used when a base event is activated.
+     *
+     * @param baseEventId the id of the base event
+     */
     void activateCalendarEntriesByBaseEventId(UUID baseEventId);
 
 }
