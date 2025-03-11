b03015c46387455c6c449ed098bcf47ff1b8d868
Luis Ribeiro <lacribeiro11@gmail.com>
2025-03-11 21:08:32 +0100

    #30 Add dynamic Springdoc configuration support
    Introduce `SpringdocProperties` to allow dynamic OpenAPI configuration using externalized properties. Updated `OpenApiConfig` to use `SpringdocProperties` for configuring API title and servers, enhancing flexibility and maintainability.

---
+++ src/main/java/casp/web/backend/configuration/SpringdocProperties.java
@@ -0,0 +1,37 @@
+package casp.web.backend.configuration;
+
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//This class is used for configuration,
+// and the setter methods populate its properties when the Springdoc-related configuration values are provided.
+@SuppressWarnings("unused")
+@Configuration
+@ConfigurationProperties(prefix = "springdoc")
+class SpringdocProperties {
+    private final Set<Server> servers = new HashSet<>();
+    private String title;
+
+    Set<Server> getServers() {
+        return servers;
+    }
+
+    void setServers(Set<Server> servers) {
+        // Clear existing servers to remove unnecessary entries
+        this.servers.clear();
+        servers.forEach(server -> server.setUrl(server.getUrl() + "/admin-v2"));
+        this.servers.addAll(servers);
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    void setTitle(final String title) {
+        this.title = title;
+    }
+}
