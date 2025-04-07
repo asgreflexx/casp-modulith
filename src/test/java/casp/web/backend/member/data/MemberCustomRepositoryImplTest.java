687ec9230511c119fdf18238b4f723f20205fc8b
Luis Ribeiro <lacribeiro11@gmail.com>
2025-04-07 19:37:58 +0200

    #43 Add member lookup excluding specific Dog ID
    Implemented a new repository method `findAllByNotDogId` to fetch members not associated with a specific Dog ID. Added corresponding logic in the repository implementation and unit tests to validate functionality.

--- src/test/java/casp/web/backend/member/data/MemberCustomRepositoryImplTest.java
+++ src/test/java/casp/web/backend/member/data/MemberCustomRepositoryImplTest.java
@@ -1,6 +1,11 @@
 package casp.web.backend.member.data;
 
 import casp.web.backend.common.enums.EntityStatus;
+import casp.web.backend.common.reference.DogHasHandlerReference;
+import casp.web.backend.common.reference.DogHasHandlerReferenceRepository;
+import casp.web.backend.common.reference.DogReference;
+import casp.web.backend.common.reference.DogReferenceRepository;
+import casp.web.backend.common.reference.MemberReference;
 import jakarta.annotation.Nullable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -25,12 +30,19 @@ class MemberCustomRepositoryImplTest {
 
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private DogReferenceRepository dogRepository;
+    @Autowired
+    private DogHasHandlerReferenceRepository dogHasHandlerRepository;
     private Member john;
     private Member doe;
 
     @BeforeEach
     void setUp() {
+        dogRepository.deleteAll();
+        dogHasHandlerRepository.deleteAll();
         memberRepository.deleteAll();
+
         john = createMember("John", "John", EntityStatus.ACTIVE, Role.ADMIN);
         doe = createMember("Doe", "Doe", EntityStatus.ACTIVE, Role.CASHIER);
         createMember("deleted", "deleted", EntityStatus.DELETED, null);
@@ -109,6 +121,45 @@ class MemberCustomRepositoryImplTest {
         void memberNotFound() {
             var memberId = UUID.randomUUID();
             assertThrows(NoSuchElementException.class, () -> memberRepository.findByIdAndEntityStatusCustom(memberId, EntityStatus.DELETED));
+        }
+    }
+
+    @Nested
+    class FindAllByNotDogId {
+        private DogReference dogReference;
+
+        @BeforeEach
+        void setUp() {
+            var johnReference = new MemberReference();
+            johnReference.setId(john.getId());
+            johnReference.setFirstName(john.getFirstName());
+            johnReference.setLastName(john.getLastName());
+            johnReference.setEmail(john.getEmail());
+            dogReference = new DogReference();
+            dogReference.setName("Bella");
+            dogReference = dogRepository.save(dogReference);
+            var dogHasHandler = new DogHasHandlerReference();
+            dogHasHandler.setDog(dogReference);
+            dogHasHandler.setMember(johnReference);
+            dogHasHandlerRepository.save(dogHasHandler);
+        }
+
+        @Test
+        void byDogId() {
+            assertThat(memberRepository.findAllByNotDogId(dogReference.getId(), null, Pageable.unpaged()))
+                    .containsExactly(doe);
+        }
+
+        @Test
+        void byDogIdAndFirstName() {
+            assertThat(memberRepository.findAllByNotDogId(dogReference.getId(), john.getFirstName(), Pageable.unpaged()))
+                    .isEmpty();
+        }
+
+        @Test
+        void byDogIdAndLastName() {
+            assertThat(memberRepository.findAllByNotDogId(dogReference.getId(), john.getLastName(), Pageable.unpaged()))
+                    .isEmpty();
         }
     }
 }
