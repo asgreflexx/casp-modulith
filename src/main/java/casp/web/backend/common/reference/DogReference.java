61a92781b480690f874fde2fbc76f33a00a53973
Luis Ribeiro <lacribeiro11@gmail.com>
2025-05-23 10:25:44 +0200

    #46 Add new attributes to DogReference model
    This commit introduces breederName, breedName, birthDate, and gender attributes to the DogReference model. These additions support more detailed information about dogs and include defaulting the gender to FEMALE.

--- src/main/java/casp/web/backend/common/reference/DogReference.java
+++ src/main/java/casp/web/backend/common/reference/DogReference.java
@@ -1,12 +1,14 @@
 package casp.web.backend.common.reference;
 
 import casp.web.backend.common.enums.EntityStatus;
+import casp.web.backend.common.enums.Gender;
 import com.querydsl.core.annotations.QueryEntity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -25,6 +27,14 @@ public class DogReference {
 
     @NotBlank
     private String ownerName;
+
+    private String breederName;
+
+    private String breedName;
+
+    private LocalDate birthDate;
+
+    private Gender gender = Gender.FEMALE;
 
     public UUID getId() {
         return id;
@@ -56,6 +66,38 @@ public class DogReference {
 
     public void setOwnerName(String ownerName) {
         this.ownerName = ownerName;
+    }
+
+    public String getBreederName() {
+        return breederName;
+    }
+
+    public void setBreederName(String breederName) {
+        this.breederName = breederName;
+    }
+
+    public String getBreedName() {
+        return breedName;
+    }
+
+    public void setBreedName(String breedName) {
+        this.breedName = breedName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
     }
 
     @Override
