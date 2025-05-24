f06451fd12f38aa0d766d503c5b907d2432cd90b
Luis Ribeiro <lacribeiro11@gmail.com>
2025-05-24 17:36:53 +0200

    #47, #48 Remove unused imports in test classes
    Eliminated unused `ResponseEntity` and `Page` imports from test files in the `DogHasHandlerRestControllerTest` and `CourseRestControllerTest` classes. This improves code readability and adheres to clean coding practices.

--- src/test/java/casp/web/backend/dog/presentation/DogHasHandlerRestControllerTest.java
+++ src/test/java/casp/web/backend/dog/presentation/DogHasHandlerRestControllerTest.java
@@ -12,7 +12,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Set;
