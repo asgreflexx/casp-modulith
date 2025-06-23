3d74194cab70b8ff670986af3de3937daa6a5d53
Luis Ribeiro <lacribeiro11@gmail.com>
2025-06-23 15:46:28 +0200

    #66 Add `findAllByParticipant` method to `ExamCustomRepository`
    Introduce `findAllByParticipant` method to `ExamCustomRepository` and its implementation to retrieve exams by participant with active status. Add corresponding unit tests to validate functionality.

--- src/main/java/casp/web/backend/calendar/data/ExamCustomRepositoryImpl.java
+++ src/main/java/casp/web/backend/calendar/data/ExamCustomRepositoryImpl.java
@@ -1,14 +1,26 @@
 package casp.web.backend.calendar.data;
 
+import casp.web.backend.calendar.data.participants.ExamParticipant;
+import casp.web.backend.common.enums.EntityStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Component;
 
 @Component
 class ExamCustomRepositoryImpl extends BaseEventCustomRepositoryImpl<Exam> implements ExamCustomRepository {
+    private static final QExam EXAM = QExam.exam;
 
     @Autowired
     ExamCustomRepositoryImpl(MongoOperations mongoOperations) {
         super(mongoOperations);
+    }
+
+    @Override
+    public Page<Exam> findAllByParticipant(ExamParticipant participant, Pageable pageable) {
+        return query()
+                .where(EXAM.participants.contains(participant), EXAM.entityStatus.eq(EntityStatus.ACTIVE))
+                .fetchPage(pageable);
     }
 }
