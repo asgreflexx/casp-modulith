a04f0969fe132a61118a12769cfc2c6af714694d
Luis Ribeiro <lacribeiro11@gmail.com>
2026-04-23 10:32:48 +0200

    #75 Refactor to PageModel

---
+++ src/main/java/casp/web/backend/configuration/WebConfig.java
@@ -0,0 +1,9 @@
+package casp.web.backend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+class WebConfig {
+}
